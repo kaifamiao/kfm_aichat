c72f69cd29e22a088035a23206c497f964f9493b
1002020905@qq.com <1002020905@qq.com>
2024-10-04 03:31:41 +0800

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <Header />
+    <div class="content">
+      <Sidebar />
+      <Main />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Header from '../components/Header.vue';
+import Sidebar from '../components/Sidebar.vue';
+import Main from '../components/Main.vue';
+import Footer from '../components/Footer.vue';
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+.content {
+  display: flex;
+  flex: 1;
+  overflow: hidden;
+}
+</style>
